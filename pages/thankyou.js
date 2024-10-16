6322e8b5191673ec502df309ea85a461cc2c3130
prajaktpatilaweb <prajaktaweb@gmail.com>
2024-10-16 15:33:32 +0530

    img to thankback webp

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -7,7 +7,7 @@ export default function Thankyou() {
             {/* <HomeHero /> */}
             <Box
                 sx={{
-            backgroundImage:'linear-gradient(rgba(1, 1, 1, 0.5), rgba(1, 1, 1, 0.5)),url("/images/backthank.JPG")',
+            backgroundImage:'linear-gradient(rgba(1, 1, 1, 0.5), rgba(1, 1, 1, 0.5)),url("/images/backthank.webp")',
                     backgroundRepeat: 'no-repeat',
                     backgroundSize: 'cover',
                     display: 'flex',
